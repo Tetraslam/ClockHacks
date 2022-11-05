c7ceb1419261c0d017f45ab81d9b8d74cb55d8ce
Tetraslam <bhowmickshresht@gmail.com>
2022-11-05 17:51:34 +0530

    data

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from eras import twenty
-
+DataFrame.index = np.arange(1, len(df) + 1)
 leaderboard = pd.DataFrame({
     'Player': ["Tetraslam", "Dr Snek", "FireTheLost"],
     'Score': [1000, 500, 100]
