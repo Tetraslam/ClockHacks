cb523db879f93f2bf38dabbbbab94179a98c03db
Tetraslam <bhowmickshresht@gmail.com>
2022-11-05 18:09:08 +0530

    if

--- main.py
+++ main.py
@@ -11,9 +11,8 @@ leaderboard = pd.DataFrame({
 with st.form(key='playerName'):
     text_input = st.text_input(label='Enter your name')
     submit_button = st.form_submit_button(label='That is indeed my name.')
-
-if submit_button:
-    st.write(f'Hello there, {playerName}')
+    if submit_button:
+        st.write(f'Hello there, {playerName}')
 
 def home():
     st.write(
