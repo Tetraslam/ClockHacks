16a3d88799a2e62435fb7de37595f05489b8bd43
Tetraslam <bhowmickshresht@gmail.com>
2022-11-06 00:13:58 +0530

    t

--- eras.py
+++ eras.py
@@ -7,6 +7,7 @@ import streamlit as st
 def twenty():
     index = 1
     score = 0
+    confirmed = False
     activeImage = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Berlinermauer.jpg/267px-Berlinermauer.jpg'
     st.write("# 20th Century")
     st.write("### Choose the year this picture was taken!")
@@ -16,23 +17,29 @@ def twenty():
         max_value=2000,
         label_visibility="collapsed",
     )  # time slider
+    if st.button('Confirm'):
+        finalx = x
     st.image(activeImage, use_column_width=True)
     while index<3:
-        if x == 1980:
+        if finalx == 1980:
                 score+=1
                 index+=1
         else:
             index+=1
         if index == 1:
+            if st.button('Confirm'):
+                finalx = x
             activeImage = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/JFK_limousine.png/640px-JFK_limousine.png'
-            if x==1963:
+            if finalx==1963:
                 score+=1
                 index+=1
             else:
                 index+=1
         if index == 2:
+            if st.button('Confirm'):
+                finalx = x
             activeImage = 'https://upload.wikimedia.org/wikipedia/en/8/86/Einstein_tongue.jpg'
-            if x==1951:
+            if finalx==1951:
                 score+=1
                 index+=1
             else:
