b68e549902dd52a18590ba2211fc52cae7ef235a
Tetraslam <bhowmickshresht@gmail.com>
2022-11-05 17:14:58 +0530

    t

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ import pandas as pd
 """
 # Hello
 """
-x = st.slider('x', min_value = 1900, max_value = 2000)  # 👈 this is a widget
+x = st.slider('x', min_value = 1900, max_value = 2000)  # time slider
 st.write(x, 'squared is', x * x)
+st.write("https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Berlinermauer.jpg/267px-Berlinermauer.jpg")
