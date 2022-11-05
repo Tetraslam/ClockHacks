729a69539b4485410cef5940f1a89dd9c9bb2d88
Tetraslam <bhowmickshresht@gmail.com>
2022-11-05 17:38:35 +0530

    co

--- eras.py
+++ eras.py
@@ -1,2 +1,4 @@
+import streamlit as st
+
 def twentiethcentury():
-    
+    st.write("# 20th Century")
--- main.py
+++ main.py
@@ -7,11 +7,22 @@ page_names_to_funcs = {
     "20th Century": twenty,
 }
 
+
 def home():
-    st.write("# Chrono-Estimater\n##### This is a simple game inspired by GeoGuessr. Which year do you think this image was taken?")
+    st.write(
+        "# Chrono-Estimater\n##### This is a simple game inspired by GeoGuessr. Which year do you think this image was taken?"
+    )
     st.write("### Choose the year this picture was taken!")
-    x = st.slider("### Choose the year this picture was taken!", min_value = 1901, max_value = 2000, label_visibility="collapsed")  # time slider
-    st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Berlinermauer.jpg/267px-Berlinermauer.jpg", use_column_width=True)
+    x = st.slider(
+        "### Choose the year this picture was taken!",
+        min_value=1901,
+        max_value=2000,
+        label_visibility="collapsed",
+    )  # time slider
+    st.image(
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Berlinermauer.jpg/267px-Berlinermauer.jpg",
+        use_column_width=True,
+    )
 
 
 ChronoEstimatr2000 = st.sidebar.selectbox("Choose a page", page_names_to_funcs.keys())
