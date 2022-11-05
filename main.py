968df60517a6856df463742ac3733694d09efca6
Tetraslam <bhowmickshresht@gmail.com>
2022-11-05 17:03:56 +0530

    t

--- main.py
+++ main.py
@@ -4,17 +4,3 @@ import pandas as pd
 """
 # Hello
 """
-
-df = pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-})
-
-df
-import numpy as np
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
-st.write("Hello there")
-
-st.line_chart(chart_data)
