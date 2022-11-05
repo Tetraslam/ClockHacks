3d755ef645fff2471731d9e2e0fe703f6e510ad2
Tetraslam <bhowmickshresht@gmail.com>
2022-11-05 17:29:03 +0530

    pic

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 def home():
     st.write("# Chrono-Estimater\n##### This is a simple game inspired by GeoGuessr. Which year do you think this image was taken?")
-    x = st.slider("### Choose the year this picture was taken!", min_value = 1901, max_value = 2000)  # time slider
+    st.write("### Choose the year this picture was taken!")
+    x = st.slider("", min_value = 1901, max_value = 2000)  # time slider
     st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Berlinermauer.jpg/267px-Berlinermauer.jpg", use_column_width=True)
 
 def hi():
