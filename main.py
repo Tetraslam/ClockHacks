b47adaaf6c3f9807a1270e2ad1e90ecebb840bb2
Tetraslam <bhowmickshresht@gmail.com>
2022-11-05 17:20:43 +0530

    markdown

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 def home():
-    st.write("Hello")
+    st.write("# Hello")
     x = st.slider('x', min_value = 1900, max_value = 2000)  # time slider
     st.write(x, 'squared is', x * x)
     st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Berlinermauer.jpg/267px-Berlinermauer.jpg")
