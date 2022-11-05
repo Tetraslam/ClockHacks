2fe0b01a0ba8ef179cc53696a6ccc6eebaa6f86e
Tetraslam <bhowmickshresht@gmail.com>
2022-11-05 17:11:49 +0530

    slider

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import pandas as pd
 """
 # Hello
 """
-x = st.slider('x')  # 👈 this is a widget
+x = st.slider('x', min_value = 1900, max_value = 2000)  # 👈 this is a widget
 st.write(x, 'squared is', x * x)
