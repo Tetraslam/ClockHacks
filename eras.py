0a2954216264358fa7f7d4810745331db01cefc9
Tetraslam <bhowmickshresht@gmail.com>
2022-11-06 16:20:49 +0530

    t

--- eras.py
+++ eras.py
@@ -46,8 +46,25 @@ def twenty():
             st.write("The correct year was 1963.")
     st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/JFK_limousine.png/640px-JFK_limousine.png", use_column_width=True)
 
+    # Third Image
+    z = st.number_input(
+        "### Choose the year this picture was taken!",
+        min_value=1901,
+        max_value=2000,
+        label_visibility="collapsed",
+        step = 1,
+        key="button3"
+    )  # time slider
+    if st.button('Confirm', key='confirmbutton3'):
+        st.write("You score " +str(calculateScore(z, 1951)))
+        if calculateScore(z, 1951) == 1000:
+            st.write("That's correct! Well done.")
+        else:
+            st.write("The correct year was 1951.")
+    st.image("https://upload.wikimedia.org/wikipedia/en/8/86/Einstein_tongue.jpg", use_column_width=True)
 
-    st.write("Your total score is " + str(calculateScore(x, 1980) + calculateScore(y, 1963)))
+
+    st.write("Your total score is " + str(calculateScore(x, 1980) + calculateScore(y, 1963) + calculateScore(z, 1951)))
 
         
         
