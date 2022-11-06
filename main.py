9c5f7c8c437320443eb2d163d6de857d285b629d
Tetraslam <bhowmickshresht@gmail.com>
2022-11-06 19:24:43 +0530

    #

--- main.py
+++ main.py
@@ -42,14 +42,15 @@ def home():
             ## Welcome to Chrono-Estimater!
 
             This is time period based game, inspired by GeoGuessr, that allows players to guess the year different photographs were taken from. Testing your knowledge of history and critical thinking skills you must piece together clues from each photo, aiming to find the corect year.
-
+            ## How to play
+            Go to the side bar on the Chrono-Estimator website and choose the desired game mode and time period.
+            
             ## Features
             Different game modes, including Easy and Hard modes.
             - Easy: This tests your recognition of famous historical pictures.
             - Hard: This forces you to use often subtle clues from the picture to figure out from which time period the picture is from.
 
-            ## How to play
-            Go to the side bar on the Chrono-Estimator website and choose the desired game mode and time period.
+            
             """
         )
 
