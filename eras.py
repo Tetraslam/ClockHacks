1ff547fba84822cb1c1872337dad92a306f250d2
Tetraslam <bhowmickshresht@gmail.com>
2022-11-06 15:39:15 +0530

    y

--- eras.py
+++ eras.py
@@ -26,7 +26,7 @@ def twenty():
     st.write("You scored " + str(score))
 
     # Second Image
-    x = st.slider(
+    y = st.slider(
         "### Choose the year this picture was taken!",
         min_value=1901,
         max_value=2000,
@@ -34,7 +34,7 @@ def twenty():
     )  # time slider
     if st.button('Confirm', key='confirmbutton'):
         year = 1963
-        score = round(1000 - (abs((year-x))*10))
+        score = round(1000 - (abs((year-y))*10))
         index+=1
     st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/JFK_limousine.png/640px-JFK_limousine.png", use_column_width=True)
     st.write("You scored " + str(score))
