b08179c20c3a42824d45b04badb966bece6a75eb
Tetraslam <bhowmickshresht@gmail.com>
2022-11-06 18:47:11 +0530

    t

--- eras.py
+++ eras.py
@@ -3,7 +3,7 @@ import math
 
 
 def calculateScore(input, year):
-    return round(100000 / (abs((1000*(year-(input+1))))))
+    return (round(10000 / (abs((1000*(year-(input+1)))))))*10
 
 
 def twenty():
