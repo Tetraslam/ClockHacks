60bc3e4a432611011c48f750373fecf6fc27de72
Tetraslam <bhowmickshresht@gmail.com>
2022-11-06 15:38:48 +0530

    t

--- eras.py
+++ eras.py
@@ -18,15 +18,28 @@ def twenty():
         max_value=2000,
         label_visibility="collapsed",
     )  # time slider
-    finalx = 0
     if st.button('Confirm', key='confirmbutton'):
         year = 1980
         score = round(1000 - (abs((year-x))*10))
         index+=1
     st.image(activeImage, use_column_width=True)
-    
-    
     st.write("You scored " + str(score))
+
+    # Second Image
+    x = st.slider(
+        "### Choose the year this picture was taken!",
+        min_value=1901,
+        max_value=2000,
+        label_visibility="collapsed",
+    )  # time slider
+    if st.button('Confirm', key='confirmbutton'):
+        year = 1963
+        score = round(1000 - (abs((year-x))*10))
+        index+=1
+    st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/JFK_limousine.png/640px-JFK_limousine.png", use_column_width=True)
+    st.write("You scored " + str(score))
+
+
         
         
     
